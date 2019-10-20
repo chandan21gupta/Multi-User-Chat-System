74679f8938a2a4c5bdc6379030dc6e83f8421157
kashinkoji <chandan18386@iiitd.ac.in>
2019-10-20 22:04:31 +0530

    server socket created

---
+++ client1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stlib.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<unistd.h>
+
+/*
+    Client side
+    1) Socket creation
+    2) Connect
+*/
+
--- server.c
+++ server.c
@@ -1,15 +1,23 @@
 #include<stdio.h>
-#include<sys/socket.h> 
+#include<stlib.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<unistd.h>
 
-int main(int argc , char *argv[])
-{
-	int socket_desc;
-	socket_desc = socket(AF_INET , SOCK_STREAM , 0);
-	
-	if (socket_desc == -1)
-	{
-		printf("Could not create socket");
-	}
-	
-	return 0;
+/*
+    Server side
+    1) Create socket
+    2) Bind the socket
+    3) Listen to the socket
+    4) Accept connections
+*/
+
+void main() {
+    int server_fd;
+    server_fd = socket(AF_INET,SOCK_STREAM,0);
+    if(server_fd <=0) {
+        perror("Socket creation failed!!");
+        exit(EXIT_STATUS);
+    }
+    
 }
